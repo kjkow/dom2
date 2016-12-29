b6100b2c45c725dfd335be400daba8b346fe7f84
Kamil.Kowalczyk <karmel92@o2.pl>
2016-12-29 12:21:19 +0100

    przejscie na dodawanie przepisu

--- klient/src/main/java/github/kjkow/kontrolery/jedzenie/KontrolerPrzepisy.java
+++ klient/src/main/java/github/kjkow/kontrolery/jedzenie/KontrolerPrzepisy.java
@@ -71,7 +71,7 @@ public class KontrolerPrzepisy extends BazowyKontroler implements Initializable 
      * @param actionEvent
      */
     public void akcja_dodaj(ActionEvent actionEvent) {
-        //TODO: impl
+        otworzNowaFormatke(new KontrolerDodajPrzepis());
     }
 
     /**
